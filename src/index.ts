5c3bf3088d2f7a3bbf7c3a52622a260d098a2d4b
Muhammad Junaid <junaid1460@gmail.com>
2020-02-05 15:20:53 +0530

    Add keep alive function

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,108 @@
 import { ClientOpts, createClient, RedisClient } from "redis";
 
 interface ICache {
-    data: any;
+  data: any;
 }
 
 interface ICachedFuncInput<T> {
-    func: () => Promise<T>;
-    keys: Array<string | number>;
-    expiresAfter: number;
+  func: () => Promise<T>;
+  keys: Array<string | number>;
+  expiresAfter: number;
 }
 
 interface ICachedFuncOutput<T> {
-    hit: boolean;
-    data: T;
+  hit: boolean;
+  data: T;
 }
 
 class CacheItem<T> {
-    constructor(
-        private redisClient: RedisClient,
-        private args: ICachedFuncInput<T>,
-    ) {}
-    public async get(): Promise<T> {
-        const { expiresAfter, func } = this.args;
-        const redisKey = this.getKey();
-        const cache = await this.redisGet(redisKey).catch((e) => undefined);
-        if (cache !== undefined) {
-            return cache as T;
+  constructor(
+    private redisClient: RedisClient,
+    private args: ICachedFuncInput<T>
+  ) {}
+  public async get(): Promise<T> {
+    const { expiresAfter, func } = this.args;
+    const redisKey = this.getKey();
+    const cache = await this.redisGet(redisKey).catch(e => undefined);
+    if (cache !== undefined) {
+      return cache as T;
+    }
+    const data = await func();
+    await this.redisSet(redisKey, data, expiresAfter);
+    return data;
+  }
+
+  public async getWithStatus(): Promise<ICachedFuncOutput<T>> {
+    const { expiresAfter, func } = this.args;
+    const redisKey = this.getKey();
+    const cache = await this.redisGet(redisKey).catch(e => undefined);
+    if (cache !== undefined) {
+      return {
+        data: cache as T,
+        hit: true
+      };
+    }
+    const data = await func();
+    await this.redisSet(redisKey, data, expiresAfter);
+    return {
+      data: data,
+      hit: false
+    };
+  }
+
+  public keepAlive() {
+    const redisKey = this.getKey();
+    const { expiresAfter } = this.args;
+    return new Promise((resolve, reject) => {
+      this.redisClient.expire(redisKey, expiresAfter, (err, data) =>
+        resolve(true)
+      );
+    });
+  }
+
+  public delete() {
+    return new Promise((resolve, reject) => {
+      this.redisClient.DEL(this.getKey(), (error, data) => {
+        resolve(data);
+      });
+    });
+  }
+
+  private redisGet(redisKey: string) {
+    return new Promise((resolve, reject) => {
+      this.redisClient.get(redisKey, (error, data) => {
+        if (error || !data) {
+          return reject();
         }
-        const data = await func();
-        await this.redisSet(redisKey, data, expiresAfter);
-        return data;
-    }
+        const cache: ICache = JSON.parse(data) as any;
+        resolve(cache.data as any);
+      });
+    });
+  }
 
-    public async getWithStatus(): Promise<ICachedFuncOutput<T>> {
-        const { expiresAfter, func } = this.args;
-        const redisKey = this.getKey();
-        const cache = await this.redisGet(redisKey).catch((e) => undefined);
-        if (cache !== undefined) {
-            return {
-                data: cache as T,
-                hit: true,
-            };
-        }
-        const data = await func();
-        await this.redisSet(redisKey, data, expiresAfter);
-        return {
-            data: data,
-            hit: false,
-        };
-    }
+  private redisSet(redisKey: string, data: any, expiresAfter: number) {
+    return new Promise((resolve, __) => {
+      const multiOperations = this.redisClient.multi();
+      const cache = JSON.stringify({ data: data } as ICache);
+      multiOperations.SETEX(redisKey, expiresAfter, cache);
+      multiOperations.exec((error, _) => {
+        return resolve(true);
+      });
+    });
+  }
 
-    public delete() {
-        return new Promise((resolve, reject) => {
-            this.redisClient.DEL(this.getKey(), (error, data) => {
-                resolve(data);
-            });
-        });
-    }
-
-    private redisGet(redisKey: string) {
-        return new Promise((resolve, reject) => {
-            this.redisClient.get(redisKey, (error, data) => {
-                if (error || !data) {
-                    return reject();
-                }
-                const cache: ICache = JSON.parse(data) as any;
-                resolve(cache.data as any);
-            });
-        });
-    }
-
-    private redisSet(redisKey: string, data: any, expiresAfter: number) {
-        return new Promise((resolve, __) => {
-            const multiOperations = this.redisClient.multi();
-            const cache = JSON.stringify({ data: data } as ICache);
-            multiOperations.SETEX(redisKey, expiresAfter, cache);
-            multiOperations.exec((error, _) => {
-                return resolve(true);
-            });
-        });
-    }
-
-    private getKey() {
-        return this.args.keys.join(":");
-    }
+  private getKey() {
+    return this.args.keys.join(":");
+  }
 }
 
 export class RedisCache {
-    public redisClient: RedisClient;
-    constructor(options: ClientOpts) {
-        this.redisClient = createClient(options);
-    }
+  public redisClient: RedisClient;
+  constructor(options: ClientOpts) {
+    this.redisClient = createClient(options);
+  }
 
-    public cached<T>(args: ICachedFuncInput<T>): CacheItem<T> {
-        return new CacheItem(this.redisClient, args);
-    }
+  public cached<T>(args: ICachedFuncInput<T>): CacheItem<T> {
+    return new CacheItem(this.redisClient, args);
+  }
 }
